b9345d3962e522a83a44dd802d7620111dbdc840
Zulfi Ismailov-Demiri <id@werkkasten.com>
2022-02-04 18:41:24 +0100

    preview

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+import $ from 'jquery';
 import './style.scss'
 
-const menuBlock = `
-<div class="sticky-menu">
-  <div class="sticky-menu--first-0">
-    <ul>
-      <li>Home</li>
-    </ul>
-    <div>Close</div>
-  </div>
-</div>
-`;
+$(document).ready(function(){
+    const menuBtn = $('.sticky-menu--nav button');
+    const stickyMenu = $('.sticky-menu');
+    const slideMenu = $('.slide-menu');
+    const slideMenuItem = $('.slide-menu-item');
+    menuBtn.click(function (e) {
+        e.preventDefault();
+        stickyMenu.toggleClass('active')
+        if(stickyMenu.hasClass('active')){
+            $(this).html('<ion-icon class="close" name="close-outline"></ion-icon>')
+        } else {
+            $(this).text('Menu')
+        }
+    })
 
-
-document.querySelector('#app').innerHTML = menuBlock;
+    const subMenuButton = $('.slide-menu--item button span');
+    const slideMenuBack = $('.slide-menu-item .back');
+    subMenuButton.click(function (){
+        slideMenu.addClass('is-open')
+        $(this).parent().siblings('.slide-menu-item').addClass('active')
+        let activeHeight = $(this).parent().siblings('ul').height();
+        $('.sticky-menu').css("height", (activeHeight + 40) + "px")
+    })
+    slideMenuBack.click(function (){
+        slideMenu.removeClass('is-open')
+        slideMenuItem.removeClass('active')
+        let activeHeight = slideMenu.height();
+        $('.sticky-menu').css("height", (activeHeight + 40) + "px")
+    })
+})
